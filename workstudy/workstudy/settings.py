1048831a207165da40e3ba3654a51ee3b5e08d48
Egesa Wenslous <wayneotemahegesa@gmail.com>
2023-01-16 23:00:49 +0300

    deployment partial changes

--- workstudy/workstudy/settings.py
+++ workstudy/workstudy/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv #type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-thto$3)-08zd&px1k!zm)7x(22c%owia6ovarfy*vy-2@#!)c7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
