499628c3966d6a1cd15dacd2d81786d59c169175
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-06-14 13:47:17 +0300

    prep for marge

--- accounts/models.py
+++ accounts/models.py
@@ -2,23 +2,24 @@ import uuid
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-
     def create_user(self, email, phone_number, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError(_("The Email must be set"))
         if not phone_number:
-            raise ValueError(_('The phone number must be set'))
+            raise ValueError(_("The phone number must be set"))
         email = self.normalize_email(email)
-        user = self.model(
-            email=email, phone_number=phone_number, **extra_fields)
+        user = self.model(email=email, phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save()
         return user
@@ -28,9 +29,9 @@ class CustomUserManager(BaseUserManager):
         Create and save a SuperUser with the given email and password.
         """
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError(_("The Email must be set"))
         if not phone_number:
-            raise ValueError(_('The phone number must be set'))
+            raise ValueError(_("The phone number must be set"))
 
         user = self.create_user(email, phone_number, password, **extra_fields)
         user.is_admin = True
@@ -44,19 +45,24 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
     priamry table to authentication with email/phone number
     """
-    email = models.EmailField(_('email address'), max_length=255, unique=True,)
+
+    email = models.EmailField(
+        _("email address"),
+        max_length=255,
+        unique=True,
+    )
     phone_number = PhoneNumberField(
-        _('phone number'), blank=True, null=False, unique=True)
-    date_joined = models.DateTimeField(
-        verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+        _("phone number"), blank=True, null=False, unique=True
+    )
+    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('phone_number',)  # type: ignore
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("phone_number",)  # type: ignore
 
     objects = CustomUserManager()
 
@@ -91,17 +97,33 @@ class Account(models.Model):
     """
     table for user details linked to the primary user table
     """
+
     account_uuid = models.UUIDField(
-        _("User ID"), primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.OneToOneField(CustomUser, verbose_name=_(
-        "Account's credentials"),
+        _("User ID"),
+        primary_key=True,
+        default=uuid.uuid4,
+        max_length=8,
+        editable=False,
+    )
+    user = models.OneToOneField(
+        CustomUser,
+        verbose_name=_("Account's credentials"),
         on_delete=models.CASCADE,
-        related_name="account")
+        related_name="account",
+    )
     first_name = models.CharField(
-        _('First name'), blank=True, null=True, max_length=100)
+        _("First name"),
+        blank=True,
+        null=True,
+        max_length=50,
+    )
     last_name = models.CharField(
-        _('Last name'), blank=True, null=True, max_length=100)
-    is_supervisor = models.BooleanField(_('supervisor'), default=False)
+        _("Last name"),
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+    is_supervisor = models.BooleanField(_("supervisor"), default=False)
 
     def __str__(self):
         return self.first_name
